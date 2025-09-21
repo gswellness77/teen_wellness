2641d4d65eaa006959c338528f66cd76624ef94d
gswellness77 <gswellness77@gmail.com>
2025-09-20 20:51:36 -0700

    Open links in a separate tab

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank" rel="nofollow noopener">
+  <a href="https://vitejs.dev" target="_blank" rel="nofollow noopener noreferrer">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="nofollow noopener">
+  <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="nofollow noopener noreferrer">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
     <h1>Hello Vite!</h1>
